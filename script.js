1a9b29a1e5ed12c1670853fe71bb09e993407e4f
Stuart Bate <bate.stuart93@gmail.com>
2023-09-27 12:20:59 -0700

    adds click event

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by Javascript";
 document.querySelector("body").appendChild(h2);
 
+const input = document.getElementById('Pretty Please Click Me');
+
+function clickAlert() {
+    alert('This is pointless');
+}
+
+input.addEventListener('click', clickAlert);
